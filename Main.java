3beb525e01347e766e567db5948bf716fbd1e06a
Patel-H <h1patel@ryerson.ca>
2015-04-24 17:43:05 -0400

    Main.java
    This is the Main class of the Game

---
+++ Main.java
@@ -0,0 +1,31 @@
+import javax.swing.*;
+
+public class Main {
+	public static int lvl=1;
+	
+	public static void main(String[] args) {
+		makeLevel();
+	}
+	
+	public static void makeLevel(){
+		if(lvl<=3){
+			GameMain.running = true;
+			new Thread(new GameMain(lvl)).start();
+		}else{
+			JFrame end = new JFrame("Game Finished");
+			JPanel endp = new JPanel();
+			JLabel endl = new JLabel("YOU FINISHED THE GAME!");
+			
+			endp.setSize(200,300);
+			endp.add(endl);
+			end.add(endp);
+			
+			endl.setFont(endl.getFont().deriveFont(18.0f));
+			
+			end.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			end.setSize(300,200);
+			end.setLocation(500, 250);
+			end.setVisible(true);
+		}
+	}
+}
